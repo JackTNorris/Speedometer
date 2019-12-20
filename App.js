a0652df6572df96d1eb861d613fdae2f77d3f0a8
jtnorris2001 <48145689+jtnorris2001@users.noreply.github.com>
2019-12-19 22:46:31 -0600

    Converted stuff to MPH

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ import {
   Text,
   StatusBar,
   Component,
-  Button
+  Button,
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import {
@@ -28,89 +28,103 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 async function requestLocationPermission() {
-try {
-  const granted = await PermissionsAndroid.request(
-    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-    {
-      title: 'Grant Location',
-      message:
-        'Location data needed for app',
-      buttonNeutral: 'Ask Me Later',
-      buttonNegative: 'Cancel',
-      buttonPositive: 'OK',
-    },
-  );
-  if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-    console.log('You can use the location');
-    return true;
-  } else {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Grant Location',
+        message: 'Location data needed for app',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the location');
+      return true;
+    } else {
+      return false;
+      console.log('Location permission denied');
+    }
+  } catch (err) {
     return false;
-    console.log('Location permission denied');
+    console.warn(err);
   }
-} catch (err) {
-  return false;
-  console.warn(err);
-}
 }
 export default class App extends React.Component {
-
   state = {
-  loading: false,
-  updatesEnabled: false,
-  speed: 0,
-  location: {}
-};
+    loading: false,
+    updatesEnabled: false,
+    speed: 0,
+    location: {},
+  };
 
-  getLocUpdate = async() =>
-  {
-        const hasLocationPermission = await requestLocationPermission();
+  getLocUpdate = async () => {
+    const hasLocationPermission = await requestLocationPermission();
 
-       if (!hasLocationPermission) return;
+    if (!hasLocationPermission) return;
 
-       this.setState({ updatesEnabled: true }, () => {
-         this.watchId = Geolocation.watchPosition(
-           (position) => {
-             this.setState({ location: position });
-             this.setState({speed: position.coords.speed});
-             //console.log(position);
-             console.log(position.coords.speed);
-           },
-           (error) => {
-             this.setState({ location: error });
-             console.log(error);
-           },
-           { enableHighAccuracy: true, distanceFilter: 0, interval: 1000, fastestInterval: 500 }
-         );
-       });
-    }
+    this.setState({updatesEnabled: true}, () => {
+      this.watchId = Geolocation.watchPosition(
+        position => {
+          this.setState({location: position});
+          this.setState({speed: position.coords.speed});
+          //console.log(position);
+          console.log(position.coords.speed);
+        },
+        error => {
+          this.setState({location: error});
+          console.log(error);
+        },
+        {
+          enableHighAccuracy: true,
+          distanceFilter: 0,
+          interval: 1000,
+          fastestInterval: 500,
+        },
+      );
+    });
+  };
 
+  stopUpdates = () => {
+    this.setState({updatesEnabled: false});
+    Geolocation.clearWatch(this.watchId);
+    this.setState({updatesEnabled: false});
+    console.log('stoppped updates');
+  };
 
-    stopUpdates = () => {
-      this.setState({updatesEnabled: false});
-      Geolocation.clearWatch(this.watchId);
-      this.setState({ updatesEnabled: false })
-      console.log("stoppped updates");
-    }
-
-
-  render(){
+  render() {
     return (
-      <View style = {{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
-        <View style = {{flex: 1}}>
-          <Text style = {{fontSize: 30, borderBottomWidth: 2}}>Speedometer Testing</Text>
+      <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
+        <View style={{flex: 1}}>
+          <Text style={{fontSize: 30, borderBottomWidth: 2}}>
+            Speedometer Testing
+          </Text>
         </View>
-        <View style = {{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <Text>MPH</Text>
-        <Text style = {{fontSize: 70}}>{Math.round(parseInt(this.state.speed) * 2.237)}</Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>MPH</Text>
+          <Text style={{fontSize: 70}}>
+            {Math.round(parseInt(this.state.speed) * 2.237)}
+          </Text>
         </View>
-        <View style = {{flex: 1, flexDirection: 'column', justifyContent: 'flex-end', marginBottom: 20}}>
-          <Button title='Measure Speed' onPress={this.getLocUpdate} />
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+            marginBottom: 20,
+          }}>
+          <Button title="Measure Speed" onPress={this.getLocUpdate} />
         </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
